91fc0a185fd33b2be4e95315df2ac343c96eeb39
David Mitchell <monch1962@gmail.com>
2022-01-20 02:01:39 +0000

    Better check description

--- my-custom-checks/AWS_labels_check.py
+++ my-custom-checks/AWS_labels_check.py
@@ -3,17 +3,21 @@ from checkov.terraform.checks.resource.base_resource_check import BaseResourceCh
 
 class AWSLabelsCheck(BaseResourceCheck):
   def __init__(self):
-    name = "Ensure AWS assets are tagged with the mandatory set of labels"
+    name = """Ensure AWS assets are tagged with the mandatory set of labels:
+    - trustlevel must have one of the following values: [high, medium, low]
+    - confidentiality must have one of the following values: [high, medium, low]
+    - integrity must have one of the following values: [high, medium, low]"""
     id = "DRAC_AWS_MANDATORY_LABELS"
     supported_resources = ['aws_*']
     categories = [CheckCategories.GENERAL_SECURITY]
     super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
     
   def scan_resource_conf(self, conf):
-    if conf.get("tags",[]):
-      env = conf["tags"][0].get("Environment",{})
-      if env in ["Development", "Staging", "Production"]:
-        return CheckResult.PASSED
+    if 'tags' in conf.keys():
+      tags = conf['tags'][0]
+      if 'integrity' in tags and 'trustlevel' in tags and 'confidentiality' in tags:
+        if tags['integrity'] in ['high', 'medium', 'low'] and tags['trustlevel'] in ['high', 'medium', 'low'] and tags['confidentiality'] in ['high', 'medium', 'low']:
+          return CheckResult.PASSED
     return CheckResult.FAILED
   
 scanner = AWSLabelsCheck()
